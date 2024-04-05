37d26206fae41d8f9ff3d838d893f54ae6823276
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-04-05 23:57:23 +0100

    add virtual to Language Schema for converting typeSystem array contents into a single string

--- models/language.js
+++ models/language.js
@@ -6,9 +6,19 @@ const LanguageSchema = new Schema({
     title: { type: String, required: true, maxLength: 40 },
     typeSystem: [{ type: String, required: true, maxLength: 40 }],
     url: { type: String, required: true },
-    version: { type: Number, required: true },
+    version: { type: String, required: true },
     released: { type: Date, required: true },
     desc: { type: String, required: true },
 });
 
-module.export = mongoose.model("Language", FrameworkSchema)
+LanguageSchema.virtual("typeSystem_list").get(function () {
+    let typeSystemToString = ''
+
+    for (let i = 0 ; i < this.typeSystem.length ; i++ ) {
+        typeSystemToString += `${this.typeSystem[i]}, `
+    }
+
+    return typeSystemToString.slice(0, typeSystemToString.length - 2)
+})
+
+module.exports = mongoose.model("Language", LanguageSchema)
