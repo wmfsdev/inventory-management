6c72d9b17fc357dfca8936ba6006400e05ea0875
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-04-11 23:58:49 +0100

    add POST and GET route handlers for tech_controller

--- routes/technologies.js
+++ routes/technologies.js
@@ -8,9 +8,17 @@ const library_controller = require("../controllers/libraryController")
 // GET home page
 router.get('/', tech_controller.index)
 
+// create GET - display form
+router.get('/languages/create', tech_controller.language_create_get)
+
+// create POST - submit form
+router.post('/languages/create', tech_controller.language_create_post)
+
 router.get('/language/:id', tech_controller.language_detail)
 
 router.get('/languages', tech_controller.language_list)
+
+
 
 
 // FRAMEWORKS
@@ -20,5 +28,6 @@ router.get('/frameworks', framework_controller.framework_list)
 // LIBRARIES
 router.get('/libraries', library_controller.library_list)
 
+router.get('/library/:id', library_controller.library_detail)
 
 module.exports = router;
