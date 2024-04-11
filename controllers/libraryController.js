b433cee8d36fc066d01fd3b560453cfdb4e0256f
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-04-11 23:50:44 +0100

    add library detail function handler

--- controllers/libraryController.js
+++ controllers/libraryController.js
@@ -9,10 +9,25 @@ exports.library_list = asyncHandler( async(req, res, next) => {
 
     const libraries = await Library.find().populate("language").exec()
 
-    console.log(libraries[0])
+    console.log(libraries)
+    console.log("RENDER LIBRARY LIST")
     
     res.render("library_list", {
         title: "Libraries",
         libraries: libraries,
+    })
+})
+
+exports.library_detail = asyncHandler( async(req, res, next) => {
+
+    const library = await Library.findById(req.params.id).populate("language").exec()
+
+    //console.log(library)
+    console.log("RENDER LIBRARY DETAIL")
+    console.log(library.language[0].urlid)
+    
+    res.render("library_detail", {
+        title: "Library Detail",
+        library: library,
     })
 })
