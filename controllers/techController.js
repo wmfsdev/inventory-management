49ca1589e961d53e3a14883477b798a752cbdd04
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-04-11 23:52:59 +0100

    add POST and GET function handlers

--- controllers/techController.js
+++ controllers/techController.js
@@ -3,7 +3,7 @@
 
 const Framework = require('../models/framework')
 const Tool = require('../models/tool')
-const Library = require('../models/tool')
+const Library = require('../models/library')
 const Language = require('../models/language')
 
 const asyncHandler = require("express-async-handler")
@@ -34,21 +34,46 @@ exports.index = asyncHandler( async(req, res, next) => {
 
 exports.language_list = asyncHandler( async(req, res, next) => {
 
-    const languages = await Language.find().exec()
-    // console.log(languages)
-
+    const languages = await Language.find()
+    
     res.render("language_list", {
-        title: "Languages",
+        title: "Languages List",
         languages: languages,
+        tech: languages[0].library
     })
 })
 
 exports.language_detail = asyncHandler( async(req, res, next) => {
 
-    const test = await Language.find().exec()
-//console.log(test)
-    res.render("test", {
-        title: "test",
-       urlid: test.urlid
+    const language = await Language.findById(req.params.id)
+    .populate({
+        path: "framework",
+        select: "title",
     })
+    .populate(
+        "library",
+        "title"
+    )
+    .exec()
+
+    console.log(language)
+
+    res.render("language_detail", {
+        title: "Languages",
+        language: language,
+    })
+})
+
+// CREATE NEW LANGUAGE
+// GET
+exports.language_create_get = asyncHandler( async(req, res, next) => {
+    res.render("language_form", {
+        title: "create"
+    })
+})
+
+// POST
+exports.language_create_post = asyncHandler( async(req, res, next) => {
+
+    const language = await Language.find()
 })
