c2ba340861fe50fc2ef73960f8145478bfb9f399
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-04-19 23:30:07 +0100

    add POST function handler for submitting new language

--- controllers/techController.js
+++ controllers/techController.js
@@ -1,4 +1,4 @@
-
+const { body, validationResult } = require("express-validator")
 // import MODELS
 
 const Framework = require('../models/framework')
@@ -68,12 +68,104 @@ exports.language_detail = asyncHandler( async(req, res, next) => {
 // GET
 exports.language_create_get = asyncHandler( async(req, res, next) => {
     res.render("language_form", {
-        title: "create"
+        title: "Add Language"
     })
 })
 
 // POST
-exports.language_create_post = asyncHandler( async(req, res, next) => {
+exports.language_create_post = [
+    // validate and sanitise
+    body("name") // check which body element name is referring
+                 // to in language_form.pug
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Please enter a name for the programming language"),
+    // body("url")
+    //     .trim()
+    //     .isLength({ min:1 })
+    //     .escape()
+    //     .withMessage("Please enter a URL for relevant documentation"),
+    body("version")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Please provide a version number for latest release"),
+    body("release_date", "Invalid date")
+        .optional({ values: "falsy" })
+        .isISO8601()
+        .toDate(),
+    body("description")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("no desc"),
+    body("framework")
+        .escape(),
 
-    const language = await Language.find()
+    asyncHandler( async(req, res, next) => {
+
+      const errors = validationResult(req)
+
+      const language = new Language({
+          title: req.body.name,
+          typeSystem: req.body.type_system,
+          version: req.body.version,
+          released: req.body.release_date,
+          desc: req.body.description,
+      })
+
+      if (!errors.isEmpty()) {
+          console.log("fail")
+          res.render("language_form", {
+              title: "Add Language",
+              language: language,
+              errors: errors.array(),
+          })
+      } else {
+        const languageExists = await Language.findOne({ title: req.body.name }).exec()
+
+        if (languageExists) {
+            res.redirect(languageExists.urlid)
+        } else if (req.body.framework) {
+
+            const framework = new Framework({
+                title: req.body.framework,
+                type: undefined,
+                url: undefined,
+                version: undefined,
+                released: undefined,
+                desc: undefined,
+                language: [],
+            })
+            console.log("pre-save", framework)
+        
+            framework.language = language._id
+            language.framework = framework._id
+
+            await framework.save()
+            await language.save()
+            console.log("post-save", framework)
+
+            // maybe redirect to the declared framework?
+        } else {
+          console.log("saving")
+          await language.save()
+          console.log("language saved")
+        }
+      }
+    }),
+]
+
+exports.language_update_get = asyncHandler( async(req, res, next) => {
+
+const language = await Language.findById(req.params.id)
+console.log(language)
+    // res.render("language_update", {
+    //     title: "Language Update"
+    // })
+})
+
+exports.language_update_post = asyncHandler( async(req, res, next) => {
+
 })
