3b055999bacbbc82e9bce44c201768dcf3db4459
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-04-19 23:32:42 +0100

    add numerous routes for controllers

--- routes/technologies.js
+++ routes/technologies.js
@@ -18,11 +18,21 @@ router.get('/language/:id', tech_controller.language_detail)
 
 router.get('/languages', tech_controller.language_list)
 
+// update GET
+router.get('/language/:id/update', tech_controller.language_update_get)
 
+// update POST
+router.post('/language/:id/update', tech_controller.language_update_post)
 
 
 // FRAMEWORKS
+router.get('/frameworks/create', framework_controller.framework_create_get)
+
+router.post('/frameworks/create', framework_controller.framework_create_post)
+
 router.get('/frameworks', framework_controller.framework_list)
+
+router.get('/framework/:id', framework_controller.framework_detail)
 
 
 // LIBRARIES
