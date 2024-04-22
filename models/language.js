004928ef3b0934bfdd9cca69b518136915c8485e
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-04-22 23:52:40 +0100

    add virtuals for id based url redirect and toISODate method for rendering correct date format

--- models/language.js
+++ models/language.js
@@ -1,15 +1,35 @@
 const mongoose = require("mongoose")
+const { DateTime } = require("luxon")
 
 const Schema = mongoose.Schema
 
 const LanguageSchema = new Schema({
     title: { type: String, required: true, maxLength: 40 },
     typeSystem: [{ type: String, required: true, maxLength: 40 }],
-    url: { type: String, required: true },
+    url: { type: String },
     version: { type: String, required: true },
     released: { type: Date, required: true },
     desc: { type: String, required: true },
+    framework: { type: Schema.Types.ObjectId, ref: "Framework" },
+    library: [{ type: Schema.Types.ObjectId, ref: "Library" }],
+    image: { type: Schema.Types.ObjectId, ref: "Image" }
 });
+
+LanguageSchema.virtual("tech_list").get(function () {
+    const arr = []
+    let techListToString = ''
+    this.library.forEach((item) => {
+        arr.push(item.title)
+    })
+    arr.forEach((item) => {
+        techListToString += `${item}, `
+    })
+    return techListToString.slice(0, techListToString.length - 2)
+})
+
+LanguageSchema.virtual("released__yyyy_mm_dd").get( function() {
+    return DateTime.fromJSDate(this.released).toISODate()
+})
 
 LanguageSchema.virtual("typeSystem_list").get(function () {
     let typeSystemToString = ''
@@ -20,5 +40,9 @@ LanguageSchema.virtual("typeSystem_list").get(function () {
 
     return typeSystemToString.slice(0, typeSystemToString.length - 2)
 })
+
+LanguageSchema.virtual("urlid").get(function () {
+    return `/technologies/language/${this._id}`;
+});
 
 module.exports = mongoose.model("Language", LanguageSchema)
