645ff9167152ae936d7c1781a223a33fd8bfb61c
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-04-25 23:50:49 +0100

    remove redundant required option for numerous schema properties in FrameworkSchema

--- models/framework.js
+++ models/framework.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema
 
 const FrameworkSchema = new Schema({
     title: { type: String, required: true, maxLength: 40 },
-    type: { type: String, required: true, maxLength: 40 },
-    url: { type: String, required: true },
-    version: { type: Number, required: true },
-    released: { type: Date, required: true },
-    desc: { type: String, required: true },
-    language: [{ type: Schema.Types.ObjectId, required: true, ref: "Language" }]
+    type: { type: String, maxLength: 40 },
+    url: { type: String, },
+    version: { type: Number, },
+    released: { type: Date, },
+    desc: { type: String, },
+    language: [{ type: Schema.Types.ObjectId, ref: "Language" }]
 });
 
 FrameworkSchema.virtual("urlid").get(function () {
