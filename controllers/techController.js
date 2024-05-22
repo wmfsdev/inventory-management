2e81e6ce6c0f958a64afa2bfd75b0c65562cd6b0
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-05-22 19:59:09 +0100

    add logic for language_update_post function handler

--- controllers/techController.js
+++ controllers/techController.js
@@ -5,10 +5,21 @@ const Framework = require('../models/framework')
 const Tool = require('../models/tool')
 const Library = require('../models/library')
 const Language = require('../models/language')
+const Image = require('../models/image')
 
 const asyncHandler = require("express-async-handler")
+const multer = require('multer')
 
-// export function handlers
+const storage = multer.memoryStorage()
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+      cb(new Error('Only JPEG and PNG files are allowed!'), false);
+    }
+};
+const upload = multer({ storage: storage, fileFilter: fileFilter, fileSize: 1000000 })
+
 
 exports.index = asyncHandler( async(req, res, next) => {
 
@@ -34,8 +45,9 @@ exports.index = asyncHandler( async(req, res, next) => {
 
 exports.language_list = asyncHandler( async(req, res, next) => {
 
-    const languages = await Language.find()
-    
+    const languages = await Language.find().populate("image", "img")
+
+    console.log(languages)
     res.render("language_list", {
         title: "Languages List",
         languages: languages,
@@ -54,29 +66,46 @@ exports.language_detail = asyncHandler( async(req, res, next) => {
         "library",
         "title"
     )
+    .populate(
+        "image",
+        "img"
+    )
     .exec()
 
-    console.log(language)
+   // console.log("language_detail", language)
+    if (language.image === null) {
+        console.log("if condition works")
+        res.render("language_detail", {
+            title: "Languages",
+            language: language,
+        })
+    } else {
+        console.log("if condition doesn't work")
+        const image = "data:image/jpg;base64," + language.image.img.data.toString("base64")
 
-    res.render("language_detail", {
-        title: "Languages",
-        language: language,
-    })
+        res.render("language_detail", {
+            title: "Languages",
+            language: language,
+            image: image
+        })
+    }
 })
 
-// CREATE NEW LANGUAGE
-// GET
+// LANGUAGE
+// CREATE: GET
 exports.language_create_get = asyncHandler( async(req, res, next) => {
+
     res.render("language_form", {
-        title: "Add Language"
+        title: "Add Language",
+        image: null,
     })
 })
 
-// POST
+// CREATE: POST
 exports.language_create_post = [
-    // validate and sanitise
-    body("name") // check which body element name is referring
-                 // to in language_form.pug
+    upload.single('upload'),
+    
+    body("name") 
         .trim()
         .isLength({ min: 1 })
         .escape()
@@ -101,71 +130,238 @@ exports.language_create_post = [
         .escape()
         .withMessage("no desc"),
     body("framework")
+        .trim()
         .escape(),
 
     asyncHandler( async(req, res, next) => {
 
-      const errors = validationResult(req)
+        const errors = validationResult(req)
 
-      const language = new Language({
-          title: req.body.name,
-          typeSystem: req.body.type_system,
-          version: req.body.version,
-          released: req.body.release_date,
-          desc: req.body.description,
-      })
+        let language = new Language({
+            title: req.body.name,
+            typeSystem: req.body.type_system,
+            version: req.body.version,
+            released: req.body.release_date,
+            desc: req.body.description,
+            image: null
+        })
+        console.log(language)
+        if (typeof(req.body.frameworkvalues) === 'string') {
+            req.body.frameworkvalues = [req.body.frameworkvalues]
+        }
 
-      if (!errors.isEmpty()) {
-          console.log("fail")
-          res.render("language_form", {
-              title: "Add Language",
-              language: language,
-              errors: errors.array(),
-          })
-      } else {
+        if (req.file) {
+            const image = new Image({
+                name: req.file.originalname,
+                img: {
+                    data: req.file.buffer,
+                    contentType: req.file.mimetype,
+                }
+            })
+            language.image = image._id
+            await image.save()
+        }
+
+        if (!errors.isEmpty()) {
+            console.log("fail")
+            res.render("language_form", {
+                title: "Add Language",
+                language: language,
+                errors: errors.array(),
+            })
+        } else {
         const languageExists = await Language.findOne({ title: req.body.name }).exec()
 
         if (languageExists) {
             res.redirect(languageExists.urlid)
-        } else if (req.body.framework) {
+        } else if (req.body.frameworkvalues) {
 
-            const framework = new Framework({
-                title: req.body.framework,
-                type: undefined,
-                url: undefined,
-                version: undefined,
-                released: undefined,
-                desc: undefined,
-                language: [],
+            req.body.frameworkvalues.forEach( async(value) => {
+
+                const framework = new Framework({
+                    title: value,
+                    type: undefined,
+                    url: undefined,
+                    version: undefined,
+                    released: undefined,
+                    desc: undefined,
+                    language: [],
+                })
+                framework.language = language._id
+                language.framework.push(framework._id) 
+
+                await framework.save()
             })
-            console.log("pre-save", framework)
-        
-            framework.language = language._id
-            language.framework = framework._id
+                await language.save()
+                res.redirect(`/technologies/language/${language._id}`) 
 
-            await framework.save()
-            await language.save()
-            console.log("post-save", framework)
-
-            // maybe redirect to the declared framework?
         } else {
-          console.log("saving")
-          await language.save()
-          console.log("language saved")
+            console.log(language)
+            await language.save()
+            res.redirect(`/technologies/language/${language._id}`) 
         }
       }
     }),
 ]
 
+// UPDATE: GET
 exports.language_update_get = asyncHandler( async(req, res, next) => {
 
-const language = await Language.findById(req.params.id)
-console.log(language)
-    // res.render("language_update", {
-    //     title: "Language Update"
-    // })
+    const language = await Language.findById(req.params.id).populate("framework", "title").populate("image", "img").exec()
+
+   // console.log(language)
+    if (language.image === null) {
+        res.render("language_form", {
+            title: "Language Update",
+            language: language
+        })
+    } else {
+        const image = "data:image/jpg;base64," + language.image.img.data.toString("base64")
+
+        res.render("language_form", {
+            title: "Language Update",
+            language: language,
+            image: image
+        })
+    } 
 })
 
-exports.language_update_post = asyncHandler( async(req, res, next) => {
+// UPDATE: POST
+exports.language_update_post = [
+    upload.single('upload'),
+    
+    body("name") 
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Please enter a name for the programming language"),
+    // body("url")
+    //     .trim()
+    //     .isLength({ min:1 })
+    //     .escape()
+    //     .withMessage("Please enter a URL for relevant documentation"),
+    body("version")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Please provide a version number for latest release"),
+    body("release_date", "Invalid date")
+        .optional({ values: "falsy" })
+        .isISO8601()
+        .toDate(),
+    body("description")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("no desc"),
+    body("framework")
+        .trim()
+        .escape(),
 
-})
+    asyncHandler( async(req, res, next) => {
+
+        const errors = validationResult(req)
+
+        let language = new Language({
+            title: req.body.name,
+            typeSystem: req.body.type_system,
+            version: req.body.version,
+            released: req.body.release_date,
+            desc: req.body.description,
+            image: null,
+            _id: req.params.id
+        })
+        
+        if (typeof(req.body.frameworkvalues) === 'string') {
+            console.log("typeof")
+            req.body.frameworkvalues = [req.body.frameworkvalues]
+        }
+
+        const findImage = await Language.findById(req.params.id)
+
+        if (req.file) {
+        console.log("file submitted")
+            if (findImage.image === null) {
+               console.log("image not found")
+                const image = new Image({
+                    name: req.file.originalname,
+                    img: {
+                        data: req.file.buffer,
+                        contentType: req.file.mimetype,
+                    }
+                })
+                language.image = image._id
+                await image.save()
+                await Language.findByIdAndUpdate(req.params.id, language)
+            } else if (findImage === undefined) {
+                const image = new Image({
+                    name: req.file.originalname,
+                    img: {
+                        data: req.file.buffer,
+                        contentType: req.file.mimetype,
+                    }
+                })
+                language.image = image._id
+                await image.save()
+                await language.save()
+            } else {
+                console.log("image found")
+                const image = new Image({
+                name: req.file.originalname,
+                img: {
+                    data: req.file.buffer,
+                    contentType: req.file.mimetype,
+                },
+                _id: req.body.image_id,
+            })
+            language.image = image._id
+
+            await Image.findByIdAndUpdate(req.body.image_id, image)
+            } 
+        } else {
+            language.image = req.body.image_id
+        }
+
+        if (!errors.isEmpty()) {
+            console.log("fail")
+            res.render("language_form", {
+                title: "Add Language",
+                language: language,
+                errors: errors.array(),
+            })
+        } else {   
+        
+            const languageExists = await Language.findOne({ title: req.body.name }).exec()
+
+            if (languageExists) {
+
+                if (!req.body.frameworkvalues) {
+                    console.log("no values")
+                    await languageExists.save()
+                    res.redirect(`/technologies/language/${language._id}`) 
+                } else {
+                    req.body.frameworkvalues.forEach( async(value) => {
+                    
+                        const framework = new Framework({
+                            title: value,
+                            type: undefined,
+                            url: undefined,
+                            version: undefined,
+                            released: undefined,
+                            desc: undefined,
+                            language: [],
+                        })
+                        framework.language = language._id
+                        
+                        languageExists.framework.push(framework._id) 
+                        console.log("LANGUAGE", languageExists)
+
+                        await framework.save()
+                        await languageExists.save()
+                        res.redirect(`/technologies/language/${language._id}`)      
+                    })
+                }
+            }
+        }
+    })
+]
